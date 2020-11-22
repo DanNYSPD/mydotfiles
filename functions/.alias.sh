403de301e50de0e451da91e1f48c32f4e6eeac6d
Daniel Hernandez FCO <daniel.hernandez.job@gmail.com>
2020-11-22 01:41:06 -0600

    added functions

--- functions/.alias.sh
+++ functions/.alias.sh
@@ -1,3 +1,5 @@
 alias ddp="psql -U postgres"
 
 alias glscommit="git diff-tree --no-commit-id --name-only -r" #followed by the commit name
+
+alias g="git"
--- functions/.functions.sh
+++ functions/.functions.sh
@@ -1,7 +1,7 @@
 mkcdir ()
 {
-    mkdir -p -- "$1" &&
-    cd -P -- "$1"
+    mkdir -p -- "$@" &&
+    cd -P -- "$_"
     
 }
 function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}";  }
@@ -46,3 +46,60 @@ function backup(){
 }
 
 
+
+#get it from https://github.com/mathiasbynens/dotfiles/blob/main/.functions
+
+# Normalize `open` across Linux, macOS, and Windows.
+# This is needed to make the `o` function (see below) cross-platform.
+if [ ! $(uname -s) = 'Darwin'  ]; then
+    if grep -q Microsoft /proc/version; then
+        # Ubuntu on Windows using the Linux subsystem
+        alias open='explorer.exe';
+    else
+        alias open='xdg-open';
+    fi
+fi
+
+# `o` with no arguments opens the current directory, otherwise opens the given
+# location
+function o() {
+    if [ $# -eq 0  ]; then
+        open .;
+    else
+        open "$@";
+    fi;
+    
+}
+
+# `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
+# the `.git` directory, listing directories first. The output gets piped into
+# `less` with options to preserve color and line numbers, unless the output is
+# small enough for one screen.
+function tre() {
+    tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
+    
+}
+# Create a data URL from a file
+function dataurl() {
+    local mimeType=$(file -b --mime-type "$1");
+    if [[ $mimeType == text/*  ]]; then
+        mimeType="${mimeType};charset=utf-8";
+    fi
+    echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')";
+    
+}
+
+# Determine size of a file or total size of a directory
+function fs() {
+    if du -b /dev/null > /dev/null 2>&1; then
+        local arg=-sbh;
+    else
+        local arg=-sh;
+    fi
+    if [[ -n "$@"  ]]; then
+        du $arg -- "$@";
+    else
+        du $arg .[^.]* ./*;
+    fi;
+    
+}
