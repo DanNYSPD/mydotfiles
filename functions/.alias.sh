4439ad7394f464b83f1a8d836d15bbcc31c934a3
Daniel Hernandez <daniel.hernandez.job@gmail.com>
2021-09-02 18:50:49 -0400

    feat(main): added new functions

--- functions/.alias.sh
+++ functions/.alias.sh
@@ -15,4 +15,12 @@ alias dc="docker-compose"
 alias dexec="docker exec -it "  # get inside bash usuallu
 alias dlog="docker logs " #the next parameter is tha name of the container
 
+alias t="tmux"
+alias tt="tmuxinator"
+alias tmuxx="tmuxinator"
 #alias cd="cd && realpath ."
+
+#list all the containers names
+alias dpsname="docker ps --format 'table {{.Names}}' | tail -n +2"
+#stops all the containers
+alias dpsname-stop='dpsname | while read cname ;do echo "$cname" ; docker stop "$cname" ;done'
