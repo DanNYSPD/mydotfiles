bb704924d0f90b7545cc54867ad72b990f1de2ff
Daniel Hernandez <daniel.hernandez.job@gmail.com>
2021-09-23 10:43:57 -0500

    added alias

--- functions/.alias.sh
+++ functions/.alias.sh
@@ -54,3 +54,20 @@ alias ta="tmux attach"
 alias postman-safe='DISABLE_WAYLAND=1 postman' #a bug in postman from snap
 #$'...' (ANSI-C-like strings)	since bash 2.0. Ref:https://wiki.bash-hackers.org/scripting/bashchanges
 alias k9slack=$'psg slac | grep "usr" | grep \'slack$\' | awk \'{print $2}\' | xargs kill -9' #https://stackoverflow.com/questions/1250079/how-to-escape-single-quotes-within-single-quoted-strings
+
+alias v='vim'
+alias s='subl'
+alias j='jobs -l'
+
+alias now='date +"%T"'
+alias nowdate='date +"%d-%m-%Y"'
+
+##https://github.com/vikaskyadav/awesome-bash-alias
+##https://javascript.plainenglish.io/5-useful-bash-aliases-to-make-you-more-productive-12c04b550479 
+#https://dev.to/ablil/my-best-shell-bash-aliases-on-linux-fedora-3598
+alias f='find . |grep '
+alias h='history|grep '
+
+
+alias ffind="find . -type f"
+alias dfind="find . -type d"
